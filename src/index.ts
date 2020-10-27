bdd98208944286987c6f17c611b2ac59d38a8a4d
Jeeyong Um <conr2d@gmail.com>
2020-10-27 08:48:12 +0000

    Export key-value store

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,16 @@ import { WalletManager } from './eosdk-wallet-manager'
 import * as WalletInterfaces from './eosdk-wallet-interfaces'
 import * as WalletErrors from './eosdk-wallet-errors'
 
-export { Wallet, WalletManager, WalletInterfaces, WalletErrors }
+import { KvStore } from './kvstore'
+import { KvStoreFileSystemBackend } from './kvstore-fs-backend'
+import { KvStoreRocksDBBackend } from './kvstore-rocksdb-backend'
+
+export {
+  Wallet,
+  WalletManager,
+  WalletInterfaces,
+  WalletErrors,
+  KvStore,
+  KvStoreFileSystemBackend,
+  KvStoreRocksDBBackend,
+}
