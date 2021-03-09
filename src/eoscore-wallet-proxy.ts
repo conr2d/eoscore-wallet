5875f5c9d54834f29fd4b0c8d39cc5c947b32318
Jeeyong Um <conr2d@gmail.com>
2021-03-09 15:36:35 +0000

    Add WalletProxy for dynamic wallet construction

---
+++ src/eoscore-wallet-proxy.ts
@@ -0,0 +1,42 @@
+import { ApiInterfaces, RpcInterfaces } from 'eosjs'
+import { Wallet } from './eoscore-wallet'
+import { KeyNotFoundError } from './eoscore-wallet-errors'
+import { digestFromSerializedData } from './eoscore-wallet-utils'
+
+class WalletProxy implements ApiInterfaces.SignatureProvider {
+
+  constructor(private wallets: Wallet[]) {}
+
+  public async getAvailableKeys(): Promise<string[]> {
+    const keys = [] as string[]
+    for (const wallet of this.wallets) {
+      if (!wallet.isLocked()) {
+        keys.push(...(await wallet.getAvailableKeys()))
+      }
+    }
+    return keys
+  }
+
+  public async sign(args: ApiInterfaces.SignatureProviderArgs): Promise<RpcInterfaces.PushTransactionArgs> {
+    const { chainId, serializedTransaction, serializedContextFreeData, requiredKeys } = args
+    const digest = digestFromSerializedData(chainId, serializedTransaction, serializedContextFreeData)
+    const signatures = [] as string[]
+    for (const key of requiredKeys) {
+      let found = false
+      for (const wallet of this.wallets) {
+        const signature = await wallet.trySignDigest(digest, key)
+        if (signature) {
+          signatures.push(signature)
+          found = true
+          break
+        }
+      }
+      if (!found) {
+        throw new KeyNotFoundError()
+      }
+    }
+    return { signatures, serializedTransaction, serializedContextFreeData }
+  }
+}
+
+export { WalletProxy }
