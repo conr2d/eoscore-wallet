0d002358f04b3913e97e3c323e83497cc0eef3ea
Jeeyong Um <conr2d@gmail.com>
2021-03-14 18:58:01 +0000

    Remove repeated await from kvstore

--- src/kvstore.ts
+++ src/kvstore.ts
@@ -4,16 +4,16 @@ import { KvStoreInMemoryBackend } from './kvstore-inmemory-backend'
 class KvStore {
   constructor(private backend: KvStoreBackend = new KvStoreInMemoryBackend()) {}
 
-  async set(key: string, value: string): Promise<void> {
-    await this.backend.set(key, value)
+  set(key: string, value: string): Promise<void> {
+    return this.backend.set(key, value)
   }
 
-  async get(key: string): Promise<string | undefined> {
-    return await this.backend.get(key)
+  get(key: string): Promise<string | undefined> {
+    return this.backend.get(key)
   }
 
-  async del(key: string): Promise<void> {
-    await this.backend.del(key)
+  del(key: string): Promise<void> {
+    return this.backend.del(key)
   }
 }
 
